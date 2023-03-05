7016151033126527fd5435c7335e742af816527d
Foxcataria <105948103+Foxcataria@users.noreply.github.com>
2023-03-05 20:46:03 +0100

    Added solution for all tasks

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -11,18 +11,65 @@ const isValid = (username)=>{ //returns boolean
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+  let validusers = users.filter((user)=>{
+    return (user.username === username && user.password === password)
+  });
+  if(validusers.length > 0){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(404).json({message: "Error logging in"});
+  }
+
+  if (authenticatedUser(username,password)) {
+    let accessToken = jwt.sign({data: {"username": username, "password": password}}, 'access', { expiresIn: 60 * 60 });
+    req.session.authorization = {accessToken,username};
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({message: "Invalid Login. Check username and password"});
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.user.data["username"];
+  const isbn = req.params.isbn;
+  const review = req.query.review;
+
+  /**
+   * We have to check if a book with the given ISBN exists.
+   */
+  if (books[isbn]) {
+    if (review) {
+      books[isbn].reviews[username] = review;
+      return res.status(200).json({message: "Successfully added/updated review"});
+    }
+  } else {
+    return res.status(404).json({message: "Please provide a valid ISBN"});
+  }
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const username = req.user.data["username"];
+  const isbn = req.params.isbn;
+
+  if (books[isbn].reviews[username]) {
+    delete books[isbn].reviews[username];
+    return res.status(200).json({message: "Successfully deleted review"});
+  } else {
+    return res.status(404).json({message: "Cannot delete review for book with given ISBN"});
+  }
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,43 +1,160 @@
 const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
+const axios = require('axios').default;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username) {
+    return res.status(404).json({message: "Please provide a username"});
+  } else if (!password) {
+    return res.status(404).json({message: "Please provide a password"});
+  }
+
+  if (!doesExist(username)) {
+    users.push({"username": username, "password": password});
+    return res.status(200).json({message: "User successfully registred. Now you can login"});
+  } else {
+    return res.status(404).json({message: "User already exists!"});
+  }
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
+  const isbn = req.params.isbn;
+
+  if (books[isbn]) {
+    res.send(books[isbn]);
+  } else {
+    return res.status(300).json({message: "No books found with the given ISBN"});
+  }
   return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+
+  if (author) {
+    const bookValues = Object.keys(books).map((key) => {
+      return books[key];
+    })
+    let filteredBooks = bookValues.filter((book) => {
+      return book.author == author;
+    });
+    return res.send(filteredBooks);
+  } else {
+    return res.status(300).json({message: "No books found with the given author"});
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+
+  if (title) {
+    const bookValues = Object.keys(books).map((key) => {
+      return books[key];
+    })
+    let filteredBooks = bookValues.filter((book) => {
+      return book.title == title;
+    });
+    return res.send(filteredBooks);
+  } else {
+    return res.status(300).json({message: "No books found with the given title"});
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+
+  if (isbn) {
+    return res.send(books[isbn]["reviews"]);
+  } else {
+    return res.status(300).json({message: "No reviews found with the given ISBN"});
+  }
 });
+
+const doesExist = (username) => {
+  let usersWithSameName = users.filter((user) => {
+    return user.username == username;
+  });
+
+  if (usersWithSameName.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+/**
+ * Code to get a list of all books by means of axios.
+ */
+axios.get('http://localhost:5001/')
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+    // DO NOTHING HERE!
+  });
+
+/**
+ * Code to get book details based on ISBN by means of
+ * axios.
+ */
+axios.get('http://localhost:5001/isbn/1')
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+    // DO NOTHING HERE!
+  });
+
+/**
+ * Code to get book details based on author by means of
+ * axios.
+ */
+axios.get('http://localhost:5001/author/Chinua Achebe')
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+    // DO NOTHING HERE
+  });
+
+/**
+ * Code to get book details based on title by means of
+ * axios.
+ */
+axios.get('http://localhost:5001/title/Things Fall Apart')
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+    // DO NOTHING HERE
+  });
 
 module.exports.general = public_users;
